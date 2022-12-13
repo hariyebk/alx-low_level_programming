f73699908c2b546d05dfafcf3852bb48240d7e4e
hariyebk <harunbekri6@gmail.com>
2022-12-13 12:31:48 -0800

    $msg

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,13 +6,6 @@
 
 int main(void)
 {
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
+putchar('_putchar');
 return (0);
 } 
