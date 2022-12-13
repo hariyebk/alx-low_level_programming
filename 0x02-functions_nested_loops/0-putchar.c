8ad850b342044dda2ac6706dac7b2b83796e700b
hariyebk <harunbekri6@gmail.com>
2022-12-13 12:22:23 -0800

    $msg

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,26 @@
-#include <stdio.h>
-
-/**
- * main - Entry point
- * Description: prints "\"_putchar"
- * Return: 0
+/*
+ * File: 0-putchar.c 
+ *
  */
 
-int main(void)
-{
-	puts("\"_putchar");
-	return (0);
-}
+ #include "main.h" 
+
+ /** 
+ * main-print putchar followed by a new line
+ * 
+ *Return :always 0
+ */
+  int main(void) 
+ {
+      _putchar('_' );
+      _putchar ('p' );
+      _putchar ('u' );
+      _putchar ('t' );
+      _putchar ('c' );
+      _putchar ('h' );
+      _putchar ('a' );
+      _putchar ('r' );
+      _putchar ('\n');
+      
+       return (0) ;
+} 
