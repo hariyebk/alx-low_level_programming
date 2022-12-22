3979e7a450020cec4b867af117538e8a922b344a
hariyebk <harunbekri6@gmail.com>
2022-12-22 05:00:49 -0800

    $msg

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,37 +2,35 @@
 
 /**
  * cap_string - function that capitalizes all words of a string.
- * @ch: Pointer to Char
+ * @s: Pointer to Char
  * Return: char.
  */
 
-char *cap_string(char *ch)
+char *cap_string(char *s)
 {
 	int i = 0;
 
-	while (*(ch + i) != '\0')
+	while (*(s + i) != '\0')
 	{
-		if (i == 0)
-			*(ch + i) = *(ch + i) - ' ';
-		if (*(ch + i) == ' ' || *(ch + i) == '\t')
-			i++;
-		else if (*(ch + i) == '\n' || *(ch + i) == ',')
-			i++;
-		else if (*(ch + i) == ';' || *(ch + i) == '.')
-			i++;
-		else if (*(ch + i) == '!' || *(ch + i) == '?')
-			i++;
-		else if (*(ch + i) == '"' || *(ch + i) == '(')
-			i++;
-		else if (*(ch + i) == ')' || *(ch + i) == '{')
-			i++;
-		else if (*(ch + i) == '}')
-			i++;
-		if (*(ch + i) >= 97 && *(ch + i) <= 122)
+		if (i == 0 && (*(s + i) >= 97 && *(s + i) <= 122))
 		{
-			*(ch + i) = *(ch + i) - ' ';
+			*(s + i) = *(s + i) - ' ';
 			i++;
 		}
+		if (*(s + i) == ' ' || *(s + i) == '\n' || *(s + i) == '\t'
+		    || *(s + i) == ',' || *(s + i) == ';' || *(s + i) == '!'
+		    || *(s + i) == '?' || *(s + i) == '"' || *(s + i) == '('
+		    || *(s + i) == ')' || *(s + i) == '{' || *(s + i) == '}'
+		    || *(s + i) == '.')
+		{
+			i++;
+			if (*(s + i) >= 97 && *(s + i) <= 122)
+			{
+				*(s + i) = *(s + i) - ' ';
+			}
+		}
+		else
+			i++;
 	}
-	return (ch);
+	return (s);
 }
