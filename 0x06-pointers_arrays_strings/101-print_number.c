d1bab1ba9b56aef3fdb54bacddb4ddcdde06eba6
hariyebk <harunbekri6@gmail.com>
2022-12-22 05:31:50 -0800

    $msg

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,29 +1,35 @@
 #include "main.h"
+
 /**
- *print_number - prints an integer.
- *only using the putchar function.
- *noarrays and pointers.
- *@n: integer to be printed.
- *
- *Return: void.
+ * print_number - prints an integer.
+ * @n: input integer.
+ * Return: no return.
  */
-void print_number(int n) {
-    // Check if number is negative
-    if (n < 0) {
-        _putchar('-');
-        n = -n;
-    }
+void print_number(int n)
+{
+	unsigned int m, d, count;
 
-    // If number is greater than 0, print out each digit
-    if (n > 0) {
-        // Get the last digit
-        char lastDigit = n % 10;
+	if (n < 0)
+	{
+		_putchar(45);
+		m = n * -1;
+	}
+	else
+	{
+		m = n;
+	}
 
-        // Recurse until only one digit is left
-        if (n / 10 != 0)
-            print_number(n / 10);
+	d = m;
+	count = 1;
 
-        // Print the last digit
-        _putchar(lastDigit + '0');
-    }
+	while (d > 9)
+	{
+		d /= 10;
+		count *= 10;
+	}
+
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(((m / count) % 10) + 48);
+	}
 }
